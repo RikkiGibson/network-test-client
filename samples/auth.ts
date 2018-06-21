ca0cbf8a7257c000ab10178cf67a2b5dc976b28f
Rikki Gibson <rigibson@microsoft.com>
2018-06-21 13:12:31 -0700

    Fix config and typo

--- samples/auth.ts
+++ samples/auth.ts
@@ -1,4 +1,3 @@
-
-export const token = "<your_token_here";
+export const token = "<your_token_here>";
 export const subscriptionId = "<your_subscription_here>";
 export const resourceGroupId = "<your_resource_group_here>";
